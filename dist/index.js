8163c88f6d41a0e45309cf324163ac732562a426
洪邦洲 <80736596+joeban0608@users.noreply.github.com>
2023-10-13 11:25:57 +0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HelloWorldStr = void 0;
-/* src/index.ts */
-const HelloWorldStr = () => {
-    return "hello word";
-};
-exports.HelloWorldStr = HelloWorldStr;
